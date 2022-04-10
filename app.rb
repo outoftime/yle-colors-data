87252f257fe5b4ca6a0cf5350318942d9c396ad4
Mat Brown <matbrown@fastmail.com>
2022-04-10 14:22:29 -0400

    API endpoints for act now data

--- app.rb
+++ app.rb
@@ -52,12 +52,58 @@ class YleColorsData < Sinatra::Base
     )
   end
 
+  get "/country/us/states" do
+    json({states:
+      states
+      .select(:slug, :name)
+      .map { |row| {slug: row.fetch(:slug), name: row.fetch(:name)} }
+      .sort_by { |state| state.fetch(:name) }})
+  end
+
+  get "/country/us/states/:state_slug/counties" do |slug|
+    state = states.select(:name, :abbreviation).where(slug:).first
+    json({
+      state: {name: state.fetch(:name)},
+      counties: covid_act_now_county_data
+      .where(state: state.fetch(:abbreviation))
+      .select(:county, :county_slug)
+      .map { |row| {name: row.fetch(:county), slug: row.fetch(:county_slug)} }
+    })
+  end
+
+  get "/country/us/states/:state_slug/counties/:county_slug/metrics" do |state_slug, county_slug|
+    state = states.select(:name, :abbreviation).where(slug: state_slug).first
+    county_data = covid_act_now_county_data
+      .select(:date, :county, :weekly_new_cases_per_100k, :test_positivity_ratio, :vaccinations_completed_ratio)
+      .where(state: state.fetch(:abbreviation), county_slug:)
+      .first
+
+    json({
+      state: {name: state.fetch(:name)},
+      county: {name: county_data.fetch(:county)},
+      metrics: county_data.slice(
+        :date,
+        :weekly_new_cases_per_100k,
+        :test_positivity_ratio,
+        :vaccinations_completed_ratio
+      )
+    })
+  end
+
   private
 
   def current_rolling_averages
     DB[:us_counties_rolling_averages].where(
       import_id: current_import_id
     )
+  end
+
+  def covid_act_now_county_data
+    DB[:covid_act_now_county_data]
+  end
+
+  def states
+    DB[:states]
   end
 
   def current_import_id
